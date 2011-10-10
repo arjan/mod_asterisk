81f09dc0ee45a055cdfcb3dd576e85e2037a4bd3
Arjan Scherpenisse <arjan@scherpenisse.net>
2011-10-10 21:10:54 +0200

    Configurable Asterisk AMI/AGI options

--- mod_asterisk.erl
+++ mod_asterisk.erl
@@ -25,13 +25,54 @@
 -mod_description("Asterisk module for handling incoming and outgoing phone calls.").
 -mod_prio(900).
 
+-behaviour(gen_server).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([start_link/1]).
+
 -include_lib("include/zotonic.hrl").
 
--export([
-         init/1
-        ]).
+-record(state, {context}).
+
+start_link(Args) when is_list(Args) ->
+    ?DEBUG("DOOO"),
+    gen_server:start_link(?MODULE, Args, []).
 
 
-init(_Context) ->
+init(Args) ->
+    {context, Context} = proplists:lookup(context, Args),
+
     ?DEBUG("Starting asterisk module"),
-    ok.
+    application:load(eastrisk),
+    application:set_env(eastrisk, mgr_host, z_convert:to_list(m_config:get_value(?MODULE, mgr_host, "localhost", Context))),
+    application:set_env(eastrisk, mgr_port, z_convert:to_integer(m_config:get_value(?MODULE, mgr_port, "5038", Context))),
+    application:set_env(eastrisk, mgr_name, z_convert:to_list(m_config:get_value(?MODULE, mgr_name, "asterisk", Context))),
+    application:set_env(eastrisk, mgr_secret, z_convert:to_list(m_config:get_value(?MODULE, mgr_secret, "asterisk", Context))),
+
+    application:set_env(eastrisk, agi_port, z_convert:to_integer(m_config:get_value(?MODULE, agi_port, "6666", Context))),
+
+    case application:start(eastrisk) of
+        ok ->
+            {ok, #state{context=Context}};
+        {error, _Reason} ->
+            z_session_manager:broadcast(#broadcast{type="error", message="Asterisk running in different context!", title="Asterisk", stay=true}, z_acl:sudo(Context)),
+            ignore
+    end.
+
+
+
+handle_call(Message, _From, State) ->
+    {stop, {unknown_call, Message}, State}.
+handle_cast(Message, State) ->
+    {stop, {unknown_cast, Message}, State}.
+handle_info(_Info, State) ->
+    {noreply, State}.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+terminate(_Reason, _State) ->
+    ?DEBUG(1111111111),
+    ?DEBUG("bye"),
+    ok = application:stop(eastrisk).
+
